ccc6cce03c11f46d962cbb74b93f16fad611bce9
tarcisio-marinho <tarcisio_marinho09@hotmail.com>
2018-02-24 04:09:32 -0300

    adding cpp code

---
+++ cpp/main.cpp
@@ -0,0 +1,23 @@
+#include "scanner.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void error(const string& message){
+    cout << "Error: " << message << endl;
+    exit(-1);
+}
+
+int main(int argc, char * argv[]){
+    if(argc < 2){
+        error("no input files\ncompilation terminated.");
+    }else{
+        FILE *f = fopen(argv[1], "r");
+        if(f == NULL){
+            string name = argv[1];
+            name += ": No such file or directory";
+            error(name);
+        }
+    }
+}
