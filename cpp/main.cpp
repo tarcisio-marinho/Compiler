59bd9328646c3427d0078c72a59c45abadefe09a
tarcisio-marinho <tarcisio_marinho09@hotmail.com>
2018-02-24 04:18:21 -0300

    updating organization

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -12,12 +12,15 @@ void error(const string& message){
 int main(int argc, char * argv[]){
     if(argc < 2){
         error("no input files\ncompilation terminated.");
+
     }else{
         FILE *f = fopen(argv[1], "r");
         if(f == NULL){
             string name = argv[1];
             name += ": No such file or directory";
             error(name);
+        
         }
+        
     }
 }
