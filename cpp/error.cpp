a23ddaa7e9114dd246663e28ad0d7cfee83aec4c
tarcisio-marinho <tarcisio_marinho09@hotmail.com>
2018-02-26 12:19:51 -0300

    update error class

--- cpp/error.cpp
+++ cpp/error.cpp
@@ -1,13 +1,16 @@
 #include "error.h"
 
 
-void Error::token_error(){
+void Error::token_error(Scanner n, std::string lexema){
     std::string linha, coluna, lex, lexema;
-    std::string error = std::string("ERRO na linha ") += linha += ", coluna " += coluna += ", ultimo token lido "  += lexema;
-    std::cout << "Erro na linha: " << std::endl; 
+    std::string error = std::string("ERRO na linha ") += std::to_string(n.n_linha) += std::string(", coluna ") 
+                        += std::to_string(n.n_coluna) += std::string(", ultimo token lido ")  += lexema;
+
+    std::cout << error << std::endl; 
     exit(-1);
 
 }
+
 
 
 void Error::sintaxe_error(){
--- cpp/error.h
+++ cpp/error.h
@@ -1,12 +1,13 @@
 #ifndef __ERROR__
 #define __ERROR__
 
+#include "scanner.h"
 #include<iostream>
 #include<string>
 
 class Error{
 public:
-    static void token_error();
+    static void token_error(Scanner n, std::string lexema);
     static void sintaxe_error();
 };
 
