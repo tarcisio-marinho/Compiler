334d5f92aca7037875592d8b0844c7746cd25cd1
tarcisio-marinho <tarcisio_marinho09@hotmail.com>
2018-02-27 12:17:19 -0300

    scanner

--- cpp/scanner.cpp
+++ cpp/scanner.cpp
@@ -1,6 +1,38 @@
 #include "scanner.h"
 
-Scanner::Scanner(){
+Scanner::Scanner(FILE * arq){
     n_linha = 1;
     n_coluna = 0;
+    caracter_lido = NULL;
+    arquivo = arq;
+
+}
+
+
+Token Scanner::scan(){
+    
+    while(caracter_lido != EOF){
+
+    }
+}
+
+
+void Scanner::incremento_l_c(){
+    
+    if(caracter_lido == '\n'){ // new line
+        n_coluna = 0; 
+        n_linha +=1;
+
+    }else if(caracter_lido == '\t'){
+        n_coluna +=4; // tab
+    
+    }else{
+        n_coluna +=1; // normal char
+    }
+}
+
+
+void Scanner::proximo_caracter(){
+    caracter_lido = (char)fgetc(arquivo);
+    incremento_l_c();
 }
--- cpp/scanner.h
+++ cpp/scanner.h
@@ -1,12 +1,20 @@
 #ifndef __SCANNER__
 #define __SCANNER__
 
+#include "error.h"
+#include "token.h"
+#include <iostream>
+
 class Scanner{
 public:
     int n_linha, n_coluna;
     char caracter_lido; 
     FILE * arquivo;
-    Scanner();
+
+
+    Scanner(FILE *arq);
+    Token scan();
+    void proximo_caracter();
     void incremento_l_c();
 };
 
