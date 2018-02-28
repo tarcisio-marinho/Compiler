5d8863071c8ba0e9c7c28b97948510ca57e987d7
tarcisio-marinho <tarcisio_marinho09@hotmail.com>
2018-02-28 00:43:14 -0300

    adding gramatica as an enum

--- cpp/token.h
+++ cpp/token.h
@@ -1,13 +1,15 @@
 #ifndef __TOKEN__
 #define __TOKEN__
 
-#include<string>
-#include<iostream>
+#include <string>
+#include <iostream>
+#include "gramatica.h"
 
 class Token{
 public:
     int codigo;
     std::string lexema;
+    Gramatica g;
     Token(int cod, std::string lex);
 };
 
