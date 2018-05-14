a2686e49d7c1e56a0e875c0e36278e791b12ff10
tarcisio-marinho <tarcisio_marinho09@hotmail.com>
2018-05-14 13:11:11 -0300

    all errors

--- src/error.cpp
+++ src/error.cpp
@@ -70,9 +70,38 @@ void Error::token_esperado_nao_encontrado(Token *t, std::string esperado, std::s
 // Semantico
 
 void Error::atribuicao_incompativel(Simbol *s, int tipo1, int tipo2){
+    std::string t1, t2;
+    if(tipo1 == 3){
+        t1 = "int";
+    }else if(tipo1 == 4){
+        t1 = "float";
+    }else if(tipo1 == 5){
+        t1 = "char";
+    }else if(tipo1 == 27){
+        t1 = "tipo int";
+    }else if(tipo1 == 28){
+        t1 = "tipo float";
+    }else if(tipo1 == 29){
+        t1 = "tipo char";
+    }
+
+    if(tipo2 == 3){
+        t2 = "int";
+    }else if(tipo2 == 4){
+        t2 = "float";
+    }else if(tipo2 == 5){
+        t2 = "char";
+    }else if(tipo2 == 27){
+        t2 = "tipo int";
+    }else if(tipo2 == 28){
+        t2 = "tipo float";
+    }else if(tipo2 == 29){
+        t2 == "tipo char";
+    }
+    
     std::string error = std::string("\033[91mERRO\033[0m: na linha: \33[93m") += std::to_string(s->t->linha) += std::string("\033[0m, coluna: \33[93m") 
-                        += std::to_string(s->t->coluna) += std::string("\033[0m. Token esperado: \033[1;32m")  
-                        += std::string("\033[0m, Token lido: \033[1;32m") += s->t->lexema += std::string("\033[0m")
+                        += std::to_string(s->t->coluna) += std::string("\033[0m. Atribuição incompatível. Encontrado: \033[1;32m")
+                        += t1 += std::string(" - ") += t2 +=std::string("\033[0m")
                         += std::string("\033[0m");
 
     std::cout << error << std::endl; 
@@ -81,9 +110,38 @@ void Error::atribuicao_incompativel(Simbol *s, int tipo1, int tipo2){
 
 
 void Error::char_nao_opera_com_outros_tipos(Simbol *s, int tipo1, int tipo2){
+    std::string t1, t2;
+    if(tipo1 == 3){
+        t1 = "int";
+    }else if(tipo1 == 4){
+        t1 = "float";
+    }else if(tipo1 == 5){
+        t1 = "char";
+    }else if(tipo1 == 27){
+        t1 = "tipo int";
+    }else if(tipo1 == 28){
+        t1 = "tipo float";
+    }else if(tipo1 == 29){
+        t1 = "tipo char";
+    }
+
+    if(tipo2 == 3){
+        t2 = "int";
+    }else if(tipo2 == 4){
+        t2 = "float";
+    }else if(tipo2 == 5){
+        t2 = "char";
+    }else if(tipo2 == 27){
+        t2 = "tipo int";
+    }else if(tipo2 == 28){
+        t2 = "tipo float";
+    }else if(tipo2 == 29){
+        t2 == "tipo char";
+    }
+
     std::string error = std::string("\033[91mERRO\033[0m: na linha: \33[93m") += std::to_string(s->t->linha) += std::string("\033[0m, coluna: \33[93m") 
                         += std::to_string(s->t->coluna) += std::string("\033[0m. Tipo char não opera com outros tipos. Encontrado: \033[1;32m")
-                        += s->t->lexema += std::string("\033[0m")
+                        += t1 += std::string(" - ") += t2 +=std::string("\033[0m")
                         += std::string("\033[0m");
 
     std::cout << error << std::endl; 
