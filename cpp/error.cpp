bca0cab3820f6db465e201cab0dfcdeec0d76fa4
tarcisio-marinho <tarcisio_marinho09@hotmail.com>
2018-05-14 10:47:10 -0300

    semantico

--- cpp/error.cpp
+++ cpp/error.cpp
@@ -72,7 +72,7 @@ void Error::token_esperado_nao_encontrado(Token *t, std::string esperado, std::s
 void Error::atribuicao_incompativel(Simbol *s1, Simbol *s2, int tipo1, int tipo2){
     std::string error = std::string("\033[91mERRO\033[0m: na linha: \33[93m") += std::to_string(t->linha) += std::string("\033[0m, coluna: \33[93m") 
                         += std::to_string(t->coluna) += std::string("\033[0m. Token esperado: \033[1;32m")  
-                        += std::string("\033[0m, Token lido: \033[1;32m") += t->lexema += std::string("\033[0m") += std::string("\nErro na funcao: \033[1;32m") += funcao 
+                        += std::string("\033[0m, Token lido: \033[1;32m") += t->lexema += std::string("\033[0m") += std::string("\nErro na funcao: \033[1;32m") 
                         += std::string("\033[0m");
 
     std::cout << error << std::endl; 
