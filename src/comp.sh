9ae836746c81a75935da744252c19280aed081aa
tarcisio-marinho <tarcisio_marinho09@hotmail.com>
2018-05-14 13:45:08 -0300

    comp.sh

--- src/comp.sh
+++ src/comp.sh
@@ -1,2 +1,2 @@
-g++ main.cpp -o main scanner.cpp token.cpp regular-exp.cpp error.cpp parser.cpp -std=c++11
+g++ main.cpp -o main scanner.cpp token.cpp regular-exp.cpp error.cpp parser.cpp simbol.cpp expressao.cpp -std=c++11
 
