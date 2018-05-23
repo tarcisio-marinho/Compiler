1b7c3247f884b57e18452851db161d39cd347c31
tarcisio-marinho <tarcisio_marinho09@hotmail.com>
2018-05-22 23:11:09 -0300

    all working fine

--- src/teste.c
+++ src/teste.c
@@ -1,7 +1,38 @@
-int main () {
-    int a;
-    int b;
-    if(a>(b - 4)){
-        b = b+ 1;
+int main() {
+    int x,y,z;
+    float h;int c;float w,k,a;
+    
+    k = x;
+    w = 5/x+y*8+5* 7;
+    /** /** /*
+       teste &% 
+    ***/
+    
+
+     
+    do{
+	
+        w = w + 1;
+	
+    }while  (y<10);
+
+
+    y = x;
+    
+    while (x>w*8) {
+       float y,a;
+        w = w +1;
+        y = x;
     }
+
+    a = 0;
+
+    if (w != x) 
+    {
+
+        //p = 10;
+    }
+
 }
+
+
