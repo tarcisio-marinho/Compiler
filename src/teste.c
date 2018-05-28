d64a75cbc181243d692b43a1e1c1a7a78c4c0aeb
tarcisio-marinho <tarcisio_marinho09@hotmail.com>
2018-05-28 20:13:15 -0300

    update teste

--- src/teste.c
+++ src/teste.c
@@ -1,8 +1,12 @@
-int main(){
-	int a;
-	a = 1;
-	if(a > 2){
-		char a;
-		a = 'c';
-	}
+int main (){
+
+    float x;
+    char char1, char2;
+    int y;
+
+
+
+    x = y + 4;
+    x = 3 + 3.4;
+    x = y + ( (3-1)+(4-2) / (5+4+(7-9)) ) * 0;
 }
